14b7e62653780f1212cc2b091cf750282d150672
pickleroot <pickleroot@protonmail.com>
2026-02-06 16:33:19 -0500

    Added new classes :D

---
+++ src/main/java/org/example/ProfessorFilter.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ProfessorFilter {
+    public Filter apply() {
+
+    }
+}
---
+++ src/main/java/org/example/Profile.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Profile {
+    private String firstName;
+    private String lastName;
+}
