467c44192fda56d7c51f43bafe7572d399a84d89
Shin <shintm22@gcc.edu>
2026-02-06 16:24:13 -0500

    added Course and Search classes

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Course {
+    public int code;
+    public String department;
+    public Professor prof;
+    public int creditHours;
+    public int capacity;
+    public enum timeslot {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    };
+}
---
+++ src/main/java/org/example/Search.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Search {
+    ArrayList<Course> results;
+    ArrayList<Filter> filters;
+    String query;
+
+
+}
