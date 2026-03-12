2c3e70693d2bd928d45956907f1afd006a62a9e3
Ina Tang <inatang75@outlook.com>
2026-03-12 00:39:03 -0400

    Implement /search and most of /filter

--- backend/src/main/java/edu/gcc/wafflehouse/Driver.java
+++ backend/src/main/java/edu/gcc/wafflehouse/Driver.java
@@ -1,6 +1,7 @@
 package edu.gcc.wafflehouse;
 
 import io.javalin.Javalin;
+import java.util.ArrayList;
 
 /**
  * Controller / Router
@@ -15,8 +16,45 @@ public class Driver {
         Student student = new Student();
         Schedule schedule = student.getSchedule();
 
-        /* TODO: Search (this doesn't work. Consider a query approach?) */
-        app.get("/course", ctx -> ctx.json(search.getCourses()));
+        // Search database
+        app.get("/search", ctx -> {
+            // Get query
+            String query = ctx.queryParam("q");
+
+            // Handle cases where the query parameter is missing or empty
+            if (query == null || query.isEmpty()) {
+                ctx.status(400); // Bad Request
+                ctx.result("Missing 'query' parameter");
+            }
+
+            // Search
+            ArrayList<Course> results = search.search(query);
+            ctx.json(results);  // return results in JSON
+        });
+
+        // Get cached search results, filtered according to the latest filter inputs
+        app.get("/filter", ctx -> {
+            // Hardcoding all the filter names and parsing their queryParams one by one
+            // NOTE: this is actually easier than using a List of filters
+            //       b/c we need 1-1 correspondence btw data and the specific filter
+            String courseName = ctx.queryParam("name");
+            String professor = ctx.queryParam("prof");
+            String department = ctx.queryParam("dept");
+            String timeslot = ctx.queryParam("time");
+            String credits = ctx.queryParam("credits");
+
+            // Set all inputs for the filters
+            search.nameFilter.setInput(courseName);
+            search.profFilter.setInput(professor);
+            // TODO: finish the rest (some also need to be implemented)
+            // TODO: (sprint 2) make it more secure by switching to private variables?
+
+            // Get filtered result
+            ArrayList<Course> results = search.getFilteredResults();
+            ctx.json(results);
+        });
+
+        app.get("/courses", ctx -> ctx.json(search.getCourses()));
 
         // Get schedule
         app.get("/schedule", ctx -> ctx.json(schedule));
